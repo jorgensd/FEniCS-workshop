39fcbc5232a3a094faea04fc0b5ea171f3a79284
jorgensd <dokken92@gmail.com>
2024-10-16 22:35:08 +0200

    Finish galerkin subsection

--- src/galerkin.py
+++ src/galerkin.py
@@ -131,11 +131,116 @@ plot_interval_basis_functions(5, 2, lambda x: x + 3 * np.sin(np.pi * x))
 # $$
 #
 
+# We rewrite the equation above as a residual
+#
+# $$
+# \begin{align*}
+# R(u) = -\frac{\partial^2 u}{\partial x^2} - f
+# \end{align*}
+# $$
+
+# **Goal is to find a $u$ such that $R(u)=0$**.
 
 # If we substitute $u_h$ into the Poisson equation, we get
 #
 # $$
 # \begin{align*}
-# -\sum_{i=0}^{N-1}u_i\frac{\partial^2 \phi_i}{\partial x^2} &= f(x) \quad \text{in} \quad \Omega = [0, 1]
+# -\sum_{i=0}^{N-1}u_i\frac{\partial^2 \phi_i}{\partial x^2} - f(x) &= 0 \quad \text{in} \quad \Omega = [0, 1]
 # \end{align*}
 # $$
+
+# ```{admonition} Does this equation make sense for piecewise linear polynomials?
+# :class: dropdown
+# As the double derivative of a linear function is $0$, we cannot directly solve this equation!
+# ```
+
+# We want to weaken the requirement of the equality
+#
+# Inner product:
+#
+# $$
+# \begin{align*}
+# (a, b) &= \int_\Omega a \cdot b ~\mathrm{d}x
+# \end{align*}
+# $$
+#
+# With this, we want to find a $u_h$ such that
+#
+# $$
+# \begin{align*}
+# (R(u_h), v) = (0, v) &= 0 \qquad \forall v \in V_h
+# \end{align*}
+# $$
+#
+# Reconsidering the equation above, we have
+#
+# $$
+# \begin{align*}
+# \int_\Omega -\frac{\partial^2 u_h}{\partial x^2}\cdot v ~\mathrm{d} x = \int_{\Omega} f\cdot v~\mathrm{d}x
+# \end{align*}
+# $$
+#
+# Integrate by parts
+#
+# $$
+# \begin{align*}
+# \int_\Omega \frac{\partial u_h}{\partial x}\cdot \frac{\partial v}{\partial x}~\mathrm{d} x - \left[\frac{\partial u_h}{\partial x}v\right]_{0}^{1} = \int_{\Omega} f\cdot v~\mathrm{d}x
+# \end{align*}
+# $$
+#
+# ## The bilinear and linear form
+# We define the **bilinear form** $a(u,v)$
+#
+# $$
+# a(u,v)= \int_\Omega \frac{\partial u_h}{\partial x}\cdot \frac{\partial v}{\partial x}~\mathrm{d} x - \left[\frac{\partial u_h}{\partial x}v\right]_{0}^{1}
+# $$
+#
+# and the **linear form** $L(v)$
+#
+# $$
+# L(v) = \int_{\Omega} f\cdot v~\mathrm{d}x
+# $$
+#
+# We can now insert our expansion of $u_h$ (where each $u_i$ is to be determined)
+#
+# $$
+# \begin{align*}
+# \sum_{i=0}^{N-1}u_i \int_\Omega \frac{\partial \phi_i}{\partial x}\frac{\partial v}{\partial x}~\mathrm{d} x - \sum_{i=0}^{N-1}u_i\left[\frac{\partial \phi_i}{\partial x}v]\right]_{0}^{1} = \int_{\Omega} f(x)v~\mathrm{d}x
+# \end{align*}
+# $$
+#
+# By choosing $v=\phi_0, \dots, \phi_{N-1}$, we can set up a linear system
+#
+# $$
+# \begin{align*}
+# Au = b
+# \end{align*}
+# $$
+#
+# where
+#
+# $$
+# \begin{align*}
+# A_{j, i} = \int_\Omega \frac{\partial \phi_i}{\partial x}\frac{\partial \phi_j}{\partial x}~\mathrm{d} x - \sum_{i=0}^{N-1}u_i\left[\frac{\partial \phi_i}{\partial x}\phi_j]\right]_{0}^{1}
+# \end{align*}
+# $$
+#
+# $$
+# \begin{align*}
+# b_{j} =  \int_{\Omega} f \phi_j~\mathrm{d}x
+# \end{align*}
+# $$
+#
+# ## Galerkin orthogonality
+#
+# A key property of this method is that if $a(u_h) = l(v)$, then the error between the exact solution $u$ and $u_h$, given as $\epsilon=u-u_h$ has the following property:
+#
+# $$
+# a(\epsilon, v) = a(u-u_h, v) = a(u, v) - a(u_h, v) = l(v) - l(v) = 0
+# $$
+#
+# Thus the error is orthogonal to the chosen sub-space
+
+# ## Questions
+# 1. How does the matrix $A$ look like? Is it dense or sparse?
+# 2. Does the Galerkin orthogonality property say something about the quality of the solution?
